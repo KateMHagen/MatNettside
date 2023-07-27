cf4c0e5f10ad31507746aea9a20371d125d57cfe
Kate <katemhagen@gmail.com>
2023-07-27 16:44:36 -0700

    Added my project

---
+++ src/Menu.jsx
@@ -0,0 +1,19 @@
+import React from "react"
+
+export default function Menu() {
+    return (
+        <div>
+            <div className="menu-container">
+                <header>
+                    <h1>Meny</h1>
+                    <p>Hjemmelaget klassiske asiatiske matretter</p>
+                </header>
+                <section>
+                    <img className="food-img" src='https://media.istockphoto.com/id/1311559425/photo/freshly-cooked-filipino-food-called-lumpiang-shanghai.jpg?s=612x612&w=0&k=20&c=84-2RxGybiGcIImRc7wsdaay7Es1GONLQDdpTbC4jLk='/>
+                    <img className="food-img" src='https://media.istockphoto.com/id/1135543257/photo/steamed-meat-dumpling.jpg?s=612x612&w=0&k=20&c=45e4NN7SpWbAbr3csIkni5U7IefYoBrYSvR0Gmb23ww='/>
+                    <img className="food-img" src='https://media.istockphoto.com/id/1135543257/photo/steamed-meat-dumpling.jpg?s=612x612&w=0&k=20&c=45e4NN7SpWbAbr3csIkni5U7IefYoBrYSvR0Gmb23ww='/>
+                </section>
+            </div>
+        </div>
+    )
+}
